b56ea872bd5e7b5ac99440e49ca1ca15d2030517
Pablo-Wiseman <106688042+Pablo-Wiseman@users.noreply.github.com>
2024-01-27 22:33:19 +0500

    post-card-init-new-screen

--- app/src/main/java/com/example/post/MainActivity.kt
+++ app/src/main/java/com/example/post/MainActivity.kt
@@ -8,11 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.post.ui.theme.PostCard
 import com.example.post.ui.theme.PostTheme
--- app/src/main/java/com/example/post/ui/theme/PostCard.kt
+++ app/src/main/java/com/example/post/ui/theme/PostCard.kt
@@ -1,23 +1,43 @@
 package com.example.post.ui.theme
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,36 +46,40 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.post.R
 
 @Preview(showBackground = true)
 @Composable
-fun PostCard(){
+fun PostCard() {
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+    ) {
 
-    Card(modifier = Modifier.fillMaxSize()) {
-        Column(modifier = Modifier
-            .padding(8.dp)
-        ) {
-            PostHeader()
-            Spacer(modifier = Modifier.padding(5.dp))
-            TitleText()
-            Spacer(modifier = Modifier.padding(5.dp))
-            Image(
+        Card {
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .clip(RoundedCornerShape(10.dp)),
-                painter = painterResource(id = R.drawable.main_pic),
-                contentScale = ContentScale.Crop,
-                contentDescription = null
-            )
-
-            Spacer(modifier = Modifier.padding(5.dp))
-            Footer()
-
+                    .padding(8.dp)
+            ) {
+                PostHeader()
+                Spacer(modifier = Modifier.padding(5.dp))
+                TitleText()
+                Spacer(modifier = Modifier.padding(5.dp))
+                Image(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(10.dp)),
+                    painter = painterResource(id = R.drawable.main_pic),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = null
+                )
+                Spacer(modifier = Modifier.padding(5.dp))
+                Footer()
+            }
         }
-
     }
 }
 
@@ -95,12 +119,12 @@ private fun PostHeader() {
 }
 
 @Composable
-private fun TitleText(){
+private fun TitleText() {
     Text(text = stringResource(R.string.template_text))
 }
 
 @Composable
-private fun Footer(){
+private fun Footer() {
     Row {
         Row(
             modifier = Modifier.weight(1f),
@@ -120,7 +144,7 @@ private fun Footer(){
 }
 
 @Composable
-private fun IconWithText(icon: Int, text: String){
+private fun IconWithText(icon: Int, text: String) {
     Row(
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -129,3 +153,5 @@ private fun IconWithText(icon: Int, text: String){
         Text(text = text)
     }
 }
+
+
